96605ef8214cd302d7dd92213f5cc609da55c16f
Vincent Scheib <scheib@chromium.org>
2015-11-05 17:55:23 -0800

    files

---
+++ device/bluetooth/bluetooth_gatt_notify_session_android.cc
@@ -0,0 +1,17 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/bluetooth/bluetooth_gatt_notify_session_android.h"
+
+namespace device {
+
+BluetoothGattNotifySessionAndroid::BluetoothGattNotifySessionAndroid(
+    const std::string& characteristic_identifier)
+    : characteristic_id_(characteristic_identifier) {
+}
+
+BluetoothGattNotifySessionAndroid::~BluetoothGattNotifySessionAndroid() {
+}
+
+}  // namespace device
