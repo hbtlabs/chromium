1b15e0f408840b8e9f64640af19bf558f970c68c
Vincent Scheib <scheib@chromium.org>
2016-01-09 20:59:43 -0800

    comments

--- device/bluetooth/android/java/src/org/chromium/device/bluetooth/ChromeBluetoothRemoteGattDescriptor.java
+++ device/bluetooth/android/java/src/org/chromium/device/bluetooth/ChromeBluetoothRemoteGattDescriptor.java
@@ -1,4 +1,4 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,8 +12,7 @@ import org.chromium.base.annotations.JNINamespace;
  * Exposes android.bluetooth.BluetoothGattDescriptor as necessary
  * for C++ device::BluetoothRemoteGattDescriptorAndroid.
  *
- * Lifetime is controlled by
- * device::BluetoothRemoteGattDescriptorAndroid.
+ * Lifetime is controlled by device::BluetoothRemoteGattDescriptorAndroid.
  */
 @JNINamespace("device")
 final class ChromeBluetoothRemoteGattDescriptor {
--- device/bluetooth/android/java/src/org/chromium/device/bluetooth/Wrappers.java
+++ device/bluetooth/android/java/src/org/chromium/device/bluetooth/Wrappers.java
@@ -452,6 +452,7 @@ class Wrappers {
 
             ArrayList<BluetoothGattDescriptorWrapper> descriptorsWrapped =
                     new ArrayList<BluetoothGattDescriptorWrapper>(descriptors.size());
+
             for (BluetoothGattDescriptor descriptor : descriptors) {
                 BluetoothGattDescriptorWrapper descriptorWrapper =
                         mDeviceWrapper.mDescriptorsToWrappers.get(descriptor);
